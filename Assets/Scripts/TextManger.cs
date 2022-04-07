2ba0209243735c625d476ba61e67e24ba7c7fec2
TheLibertador <59313807+TheLibertador@users.noreply.github.com>
2022-04-07 13:02:04 +0300

    İstenilenleri yaptım

--- Assets/Scripts/TextManger.cs
+++ Assets/Scripts/TextManger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,31 @@ using UnityEngine;
 public class TextManger : MonoBehaviour
 {
     [SerializeField] private GameObject prompterText;
-
-
+    [SerializeField] private GameObject nextBubbleText;
+    [SerializeField] private GameObject[] buttonText = new GameObject[2];
     public void OpenPrompter()
     {
         Debug.Log("Button is pushed");
         prompterText.SetActive(true);
+        for (int i = 0; i < buttonText.Length; i++)
+        {
+            buttonText[i].SetActive(false);
+        }
+        nextBubbleText.SetActive(true);
+    }
+
+
+    private void Start()
+    {
+        StartCoroutine(WaitForBubbleText());
+    }
+
+    private IEnumerator WaitForBubbleText()
+    {
+        yield return new WaitForSeconds(2);
+        for (int i = 0; i < buttonText.Length; i++)
+        {
+            buttonText[i].transform.GetChild(0).gameObject.SetActive(true);
+        }
     }
 }
